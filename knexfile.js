fb10b520d7f4f18a988b80924d4ad1dad0c2a940
Anneke McGrady <annekemcgrady@gmail.com>
2019-08-16 10:04:21 -0600

    Update environment

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,14 @@ module.exports = {
   },
   seeds: {
     directory: './seeds/dev'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 
 };
